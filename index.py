3186ef80998672188b2571188912b3c2cd05d5fd
Swayam Thakkar <swayam.r.thakkar@gmail.com>
2026-09-24 00:10:18 +0000

    commit

---
+++ index.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, send_file
+import firebase_admin
+from firebase_admin import credentials, db
+
+app = Flask(__name__)
+
+# Initialize Firebase
+cred = credentials.Certificate("firebase-key.json")
+
+firebase_admin.initialize_app(cred, {
+    "databaseURL": "https://newsletter-a2313-default-rtdb.firebaseio.com"
+})
+
+
+@app.route('/')
+def home():
+    return send_file("index.html")
+
+
+@app.route('/run-task', methods=['POST'])
+def run_task():
+    email = request.form.get('email')
+
+    # Add email to Firebase
+    ref = db.reference("subscribers")
+    ref.push({
+        "email": email
+    })
+
+    return "Successfully subscribed!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
